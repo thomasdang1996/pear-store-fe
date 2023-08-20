ed1235ae74f912b276b47ee6ef728bdda15a14f3
thomasdang1996 <thomasdang1996@gmail.com>
2023-08-20 15:36:49 +0200

    udpdate-dropdown: updated dropdown, fixed shopping cart refreshing

--- src/utils/OutsideClickEventListener.jsx
+++ src/utils/OutsideClickEventListener.jsx
@@ -1,60 +1,38 @@
 import { useEffect, useRef } from "react"
 
-function listenToDropdown() {
-  document.addEventListener('click', event => {
-    const isDropdownButton = event.target.matches(".user-button")
-
-    // if it is not a drop down button, but an element inside a drop down button, ignore it
-    if (!isDropdownButton && event.target.closest(".drop-down") != null) {
-      return
-    }
-
-    let currentDropdown
-    if (!isDropdownButton) {
-      currentDropdown = event.target.closest(".drop-down")
-
-      // if the 'active' token of the elements exists, it removes it
-      // from the list and returns false, if it doesn't exist, it is added and returns true
-      currentDropdown.classList.toggle('active')
-    }
-
-
-    // will iterate through all dropdowns and will keep the current dropdown open
-    document
-      .querySelectorAll('.drop-down.active')
-      .forEach(dropdown => {
-        if (dropdown === currentDropdown) {
-          return
-        }
-        dropdown.classList.remove('active')
-      })
-  })
-}
-
-export function handleOutsideClick(callbackFunction) {
+export function handleOutsideClick(dropDownButton, dropDownElement, ...ignoredElements) {
   const ref = useRef()
   useEffect(() => {
-    document.addEventListener('click', (event) => {
-      const clickedOutside = ref.current && (!ref.current.contains(event.target))
+    const handleEvent = event => {
+      const isDropdownButton = event.target.closest(dropDownButton) != null
+      const isDropdownElement = event.target.closest(dropDownElement) != null
+      const isIgnoredElement =
+        ignoredElements != null &&
+        ignoredElements
+          .map(element => event.target.className == element)
+          .includes(true)
 
-      const clickedOnDropdownElement = event.target.closest(".cart-dropdown") != null
+      let currentDropdown
 
-      // The delete button disappears right after being
-      // clicked, so the 'clickedOnDropDownElement' 
-      // validation won't work (drop-down won't have remove element)
-      const isDeleteButton = event.target.className == 'remove-button'
-
-      if (
-        clickedOutside
-        && !clickedOnDropdownElement
-        && !isDeleteButton
-      ) {
-        callbackFunction()
+      // dropdown button toggling
+      // if the 'active' token of the elements exists, it removes it
+      // from the list and returns false, if it doesn't exist, it is added and returns true
+      if (isDropdownButton) {
+        currentDropdown = event.target.closest(dropDownElement)
+        currentDropdown.classList.toggle('active')
       }
-    })
-
+      else if (
+        !isDropdownElement &&
+        !isIgnoredElement) {
+        currentDropdown = document.querySelectorAll(dropDownElement)
+        Array.from(currentDropdown).forEach(element =>
+          element.classList.remove('active')
+        )
+      }
+    }
+    document.addEventListener('click', handleEvent)
     return () =>
       document.removeEventListener('click', handleEvent)  //to avoid memory leaks
   }, [])
-  return ref;
+  return ref
 }
